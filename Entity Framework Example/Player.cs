af4e67cf8fe869e3e6b93567cfb616bec1fab83b
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-10 14:07:25 +0200

    Aggiungere i file di progetto.

---
+++ Entity Framework Example/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_Example
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Score { get; set; }
+        public int PlayedGames { get; set; }
+        public int Victories { get; set; }
+
+
+        public Player(int playerId, string name, string surname, int score, int playedGames, int victories)
+        {
+            PlayerId = playerId;
+            Name = name;
+            Surname = surname;
+            Score = score;
+            PlayedGames = playedGames;
+            Victories = victories;
+        }
+    }
+}
